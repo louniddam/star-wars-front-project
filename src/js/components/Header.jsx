a567192d9d6a08928583528de877a2d808bfb906
Niddam <louniddam@gmail.com>
2021-11-11 19:44:55 +0100

    Header fixed

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -5,31 +5,37 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import HeaderIcon from "../../assets/headerIcon.png"
 import { Stack } from "@mui/material"
 import { useNavigate } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux'
-
+import { xsrfToken, accessToken, refreshToken, userInfos } from '../../features/auth/authSlice'
 
 
 const Header = () => {
 
     let navigate = useNavigate();
+    const dispatch = useDispatch();
     const username = useSelector(state => state.auth.userInfos.username)
+
+    const handleLogout = () => {
+      dispatch(xsrfToken(""))
+      dispatch(accessToken(""))
+      dispatch(refreshToken(""))
+      dispatch(userInfos({
+        username: "",
+        lastname: "",
+        firstname: "",
+        email: ""
+      }))
+      navigate("/")
+    }
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+            <img src={HeaderIcon} style={{ height: "auto", width: "100px" , marginBottom: 5, marginRight: 30 }} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => navigate("/")} style={{ cursor: "pointer" }}>
             StarWars
           </Typography>
@@ -37,7 +43,7 @@ const Header = () => {
             username ?
             <Stack spacing={1} direction="row" justifyContent="center" alignItems="center">
               <Typography onClick={() => navigate("/profil")} style={{ cursor: "pointer" }}>{username}</Typography>
-              <Button color="inherit" onClick={() => navigate("/")}>Logout</Button>
+              <Button color="inherit" onClick={() => handleLogout()}>Logout</Button>
             </Stack>
             :
             <Button color="inherit" onClick={() => navigate("/login")}>Login</Button>
--- src/js/pages/Profil.jsx
+++ src/js/pages/Profil.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import {Stack, TextField, Grid, Typography, Button} from '@mui/material';
-import { useNavigate } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux'
+import RobotIcon from "../../assets/robotIcon.png"
 
 const Profil = () => {
 
-    let navigate = useNavigate();
+    const username = useSelector(state => state.auth.userInfos.username)
+    const firstname = useSelector(state => state.auth.userInfos.firstname)
+    const lastname = useSelector(state => state.auth.userInfos.lastname)
+    const email = useSelector(state => state.auth.userInfos.email)
 
     return (
-        <Stack>
-            <p>Profil</p>
+        <Stack spacing={2} style={{ height: "100vh" }} justifyContent="space-around" alignItems="center">
+            <Stack>
+                <Typography variant="h3">Profil</Typography>
+            </Stack>
+            <Stack spacing={2} justifyContent="center" alignItems="center">
+                <img src={RobotIcon} style={{ width: 150, height: 150 }} />
+                <Typography variant="body1">{`Username: ${username} `}</Typography>
+                <Typography variant="body1">{`Firstname: ${firstname} `}</Typography>
+                <Typography variant="body1">{`Lastname: ${lastname} `}</Typography>
+                <Typography variant="body1">{`Email: ${email} `}</Typography>
+            </Stack>
         </Stack>
     );
 }
